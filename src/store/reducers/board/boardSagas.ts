d5d750312d6bbf6eafb57aa49c1844f806624069
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-02-02 00:56:26 +0300

    FR-16 Add stabs sagas

---
+++ src/store/reducers/board/boardSagas.ts
@@ -0,0 +1,35 @@
+import { all, takeEvery } from 'redux-saga/effects';
+import { boardActions } from './boardActions';
+
+export function* createGroupSaga(
+  action: ReturnType<typeof boardActions.createGroup>,
+) {
+  console.log(action);
+}
+
+export function* deleteGroupSaga(
+  action: ReturnType<typeof boardActions.deleteGroup>,
+) {
+  console.log(action);
+}
+
+export function* toggleActivePointSaga(
+  action: ReturnType<typeof boardActions.toggleActivePoint>,
+) {
+  console.log(action);
+}
+
+export function* updateGroupSaga(
+  action: ReturnType<typeof boardActions.updateGroup>,
+) {
+  console.log(action);
+}
+
+export function* boardMainSaga() {
+  yield all([
+    takeEvery(boardActions.createGroup, createGroupSaga),
+    takeEvery(boardActions.deleteGroup, deleteGroupSaga),
+    takeEvery(boardActions.toggleActivePoint, toggleActivePointSaga),
+    takeEvery(boardActions.updateGroup, updateGroupSaga),
+  ]);
+}
