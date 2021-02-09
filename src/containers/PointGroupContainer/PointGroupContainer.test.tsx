4e35dd8f4809c7f1b866d560fe1402bd5f37d31c
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-02-09 21:38:11 +0300

    FR-16 Update PointGroupContainer's tests

--- src/containers/PointGroupContainer/PointGroupContainer.test.tsx
+++ src/containers/PointGroupContainer/PointGroupContainer.test.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { Formik, FormikProps } from 'formik';
 import { act } from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
 
-import { IPointGroup, IPointGroupForm } from '../../interfaces';
-import { PointGroup } from '../../components/PointGroup';
+import { store } from '../../store';
+import { PointGroup } from '../../components';
 import {
-  PointGroupContainer,
+  PointGroupContainerPure,
   IPointGroupContainerProps,
 } from './PointGroupContainer';
+
+import { IPointGroup, IPointGroupForm } from '../../interfaces';
 
 type IWrapperProps = Partial<IPointGroupContainerProps>;
 
 const pointsExample = [
   {
-    id: 1,
+    id: 'p-1',
     isActive: false,
     name: 'testName',
-    pointGroupId: '1',
+    pointGroupId: 'g-2',
   },
 ];
 
@@ -25,18 +28,22 @@ const pointGroupExample: IPointGroup = {
   name: 'testName',
   points: pointsExample,
   description: 'testDescription',
-  id: '1',
+  id: 'g-2',
 };
 
 const getWrapper = (props: IWrapperProps) => {
   return mount(
-    <PointGroupContainer
-      isEditMode={false}
-      isLoading={false}
-      setEditMode={jest.fn()}
-      {...pointGroupExample}
-      {...props}
-    />,
+    <Provider store={store}>
+      <PointGroupContainerPure
+        isEditMode={false}
+        isLoading={false}
+        setEditMode={jest.fn()}
+        resetEditMode={jest.fn()}
+        deleteGroup={jest.fn()}
+        {...pointGroupExample}
+        {...props}
+      />
+    </Provider>,
   );
 };
 
@@ -68,7 +75,7 @@ describe('<PointGroupContainer />', () => {
     });
 
     expect(formikRef.current?.values.points).toHaveLength(2);
-    expect(setEditModeMock).toHaveBeenCalledWith('1');
+    expect(setEditModeMock).toHaveBeenCalledWith('g-2');
   });
 
   it('should call setEditMode by onEdit calling', () => {
@@ -81,6 +88,36 @@ describe('<PointGroupContainer />', () => {
       wrapper.find(PointGroup).prop<() => void>('onEdit')();
     });
 
-    expect(setEditModeMock).toHaveBeenCalledWith('1');
+    expect(setEditModeMock).toHaveBeenCalledWith('g-2');
+  });
+
+  it('should call deleteGroup with current id by onDelete calling', () => {
+    const onDeleteMock = jest.fn();
+    const wrapper = getWrapper({
+      deleteGroup: onDeleteMock,
+    });
+
+    act(() => {
+      wrapper.find(PointGroup).prop<() => void>('onDelete')();
+    });
+
+    expect(onDeleteMock).toHaveBeenCalledWith('g-2');
+  });
+
+  it('should call setEditMode by onAddPoint calling', (done) => {
+    const setEditModeMock = jest.fn();
+    const wrapper = getWrapper({
+      setEditMode: setEditModeMock,
+    });
+
+    act(() => {
+      wrapper.find(PointGroup).prop<() => void>('onAddPoint')();
+      wrapper.update();
+    });
+
+    setImmediate(() => {
+      expect(setEditModeMock).toHaveBeenCalled();
+      done();
+    });
   });
 });
